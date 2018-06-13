9f923b89f18bd63c3ebdbbc3f08ff9b28b0771ba
burykot <stadopikseli@gmail.com>
2018-06-13 22:36:15 +0200

    updated sources

--- src/components/posts-item.js
+++ src/components/posts-item.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 
 class PostsItem extends Component {
   postImageStyle(a) {
-    return {'background-image': 'url(' + a.image + ')'}
+    return a.image ? {'backgroundImage': 'url(' + a.image + ')'} : {'backgroundColor': '#666'};
   }
   render () {
     return <div className="post">
       <a href={this.props.post.url}>
         <div className="post-image" style={this.postImageStyle(this.props.post)}>
         </div>
-      <div class="postDescriptionWrapper">
+      <div className="postDescriptionWrapper">
         <h2>{this.props.post.title}</h2>
         <p className="post-description">{this.props.post.shortdesc}</p>
         {/* <span className="post-date">{this.props.post.date.slice(0, 10)}</span><a href={this.props.post.url} className="readmore">read more ></a>*/}

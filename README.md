3bde2fdaf787c4783ea52ac5ff367bd8118243f0
burykot <stadopikseli@gmail.com>
2018-06-29 21:23:25 +0200

    build 1 preparation

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 The idea behind this project is to create a simple site that summarizes some of my quadcopter build logs, reviews, articles and tutorials and points users to my profiles in social networks.
 
+Check out [theMessIMade](https://burykot.github.io/themessimade/).
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
--- src/components/videos-player.js
+++ src/components/videos-player.js
@@ -15,16 +15,16 @@ class VideoPlayer extends Component {
     this.setState({ style })
   }
 
-  componentWillUnmount() {
-    const style = {animation: 'fullFadeOut 0.5s'}
-    this.setState({ style })
-    // delay required
-  }
+  // componentWillUnmount() {
+  //   const style = {animation: 'fullFadeOut 0.5s'}
+  //   this.setState({ style })
+  //   // delay required on parent component instead
+  // }
 
   render () {
     return <div className="overlay" style={this.state.style} onClick={this.props.videoPlayerCloseHandler}>
       <button className="closeOverlay" > x </button>
-      <iframe src={'http://www.youtube.com/embed/' + this.props.video + '/?autoplay=1'} title="Video player" frameBorder="0" allowFullScreen></iframe>
+      <iframe src={'https://www.youtube.com/embed/' + this.props.video + '/?autoplay=1'} title="Video player" frameBorder="0" allowFullScreen></iframe>
     </div>
   }
 }
